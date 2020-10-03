5fd2bf2af8f26e26b860cfb1f8109f0538ac4e56
hari_krishnan4274 <harikrishnanv666@gmail.com>
2020-10-03 17:59:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ScaleGestureDetector
+import androidx.camera.core.CameraX
+
